501d33fa25fb172c82f2d9f8468eb55538f511e6
Doug Gabehart <dgabehar@gmail.com>
2021-02-28 11:10:46 -0600

    Initial commit of virtual integrated door

---
+++ virtual-integrated-door/virtual-integrated-door-app.groovy
@@ -0,0 +1,82 @@
+/**
+
+ */
+
+definition(
+    name: 'Virtual Integrated Door Device',
+    namespace: 'vidd',
+    author: 'Doug Gabehart',
+    description: 'An integrated lock and contact sensor',
+    category: 'Convenience',
+    iconUrl: '',
+    iconX2Url: ''
+)
+
+preferences {
+    section('Choose the lock device associated with this door') {
+        input 'lock', 'capability.lock', title: 'Door lock', required: true
+    }
+
+    section('Choose the contact sensor that detects whether the door is opened or closed') {
+        input 'doorSensor', 'capability.contactSensor', title: 'Contact sensor attached to the door', required: true
+    }
+    section('Choose the Virtual integrated door device ') {
+        input 'virtualLock', 'capability.lock', title: 'Virtual integrated door device', required: true
+        input 'virtualContactSensor', 'capability.contactSensor', title: 'Same Virtual integrated door device', required: true
+    }
+}
+
+def installed() {
+    installer()
+}
+def updated() {
+    unsubscribe()
+    installer()
+}
+def installer() {
+    log.info('Ensuring door is locked during install')
+    lock.lock()
+    virtualLockSetter('locked')
+    log.info('Assuming virtual door contact sensor is closed')
+    virtualContactSensorSetter('closed')
+
+    subscribe(doorSensor, 'contact', doorSensorHandler)
+    subscribe(lock, 'lock', lockHandler)
+    subscribe(virtualLock, 'lock', virtualLockHandler)
+}
+def doorSensorHandler(event) {
+    virtualContactSensorSetter(event.value)
+}
+def virtualLockHandler(event) {
+    log.info('Handling lock event')
+    if ('unlocking' == event.value) {
+        lock.unlock()
+    }
+    else if ('locking' == event.value) {
+        lock.lock()
+    }
+}
+
+def lockHandler(event) {
+    virtualLockSetter(event.value)
+}
+private def virtualLockSetter(value) {
+    log.info("Lock event with value $value")
+
+    if (value == 'locked' || value == 'locking') {
+        virtualLock.lock(value)
+    }
+    else if (value == 'unlocked' || value == 'unlocking') {
+        virtualLock.unlock(value)
+    }
+}
+private virtualContactSensorSetter(value) {
+    log.info("Handling door sensor event: $value")
+
+    if ('open' == value) {
+        virtualContactSensor.open(value)
+    }
+    else if ('closed' == value) {
+        virtualContactSensor.close(value)
+    }
+}
---
+++ virtual-integrated-door/virtual-integrated-door-device.groovy
@@ -0,0 +1,47 @@
+/**
+ *  Virtual integrated door device
+ */
+metadata {
+    definition (name: "Virtual integrated door device", namespace: "vidd", author: "Doug Gabehart") {          
+        capability "Lock"
+        capability "Contact Sensor"
+        capability "Sensor"
+        capability "Door Control"
+        capability "Garage Door Control"
+    }
+    tiles {                        
+        standardTile("contact", "device.contact", width: 1, height: 1) {
+            state("closed", label:'${name}', icon:"st.contact.contact.closed", backgroundColor:"#00A0DC")
+            state("open", label:'${name}', icon:"st.contact.contact.open", backgroundColor:"#e86d13")
+        }        
+        main "toggle"
+        details(["toggle", "lock", "unlock", "contact"])
+    }    
+}
+
+def lock(value) {
+    def eventValue = (value != null) ? "$value" : "locking"
+    log.info("lock value is $value")
+    sendEvent(name: "lock", value: eventValue)
+}
+
+def unlock(value) {
+    def eventValue = (value != null) ? "$value" : "unlocking"
+    log.info("unlock value is $value")
+    sendEvent(name: "lock", value: eventValue)
+}
+
+def open(value) {
+    def eventValue = (value != null) ? "$value" : "opening"
+    log.info("open value is $value")
+    sendEvent(name: "contact", value: "open")
+    sendEvent(name: "door", value: "open")
+}
+
+def close(value) {
+    def eventValue = (value != null) ? "$value" : "closing"
+    log.info("close value is $value")
+    sendEvent(name: "contact", value: "closed")
+    sendEvent(name: "door", value: "closed")
+}
+
